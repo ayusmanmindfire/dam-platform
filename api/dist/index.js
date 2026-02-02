24d98151d2df278a2d0522a1a211d6ffa9a653a7
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2026-02-02 12:29:05 +0530

    feat: setup basic route

--- api/dist/index.js
+++ api/dist/index.js
@@ -1,4 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello from DAM Platform API");
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.get("/", (_, res) => {
+    res.send("OK");
+});
+app.listen(3000);
+console.log("API server is running on http://localhost:3000");
 //# sourceMappingURL=index.js.map
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello from DAM Platform API");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (_, res) => {
+  res.send("OK");
+});
+
+app.listen(3000);
+console.log("API server is running on http://localhost:3000");
